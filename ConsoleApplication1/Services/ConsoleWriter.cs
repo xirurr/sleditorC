ff2d5336cd717c3f2823f15ea2cd1ab1af677fa4
y.egorov <y.egorov@systtech.ru>
2020-02-25 23:31:24 +0200

    цвет текста

---
+++ ConsoleApplication1/Services/ConsoleWriter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApplication1.Services
+{
+    public class ConsoleWriter
+    {
+       public static void WriteError(string message)
+        {
+            Console.ForegroundColor  = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor(); 
+        }
+       
+       public static void WriteSuccess(string message)
+       {
+           Console.ForegroundColor  = ConsoleColor.Green;
+           Console.WriteLine(message);
+           Console.ResetColor(); 
+       }
+       
+       public static void WriteDanger(string message)
+       {
+           Console.ForegroundColor  = ConsoleColor.Yellow;
+           Console.WriteLine(message);
+           Console.ResetColor(); 
+       }
+    }
+}
